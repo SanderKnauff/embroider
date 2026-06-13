745f0175f240987a534c19ef3732c0c9cc8103b0
Sander Knauff <sanderknauff@hotmail.com>
2026-06-13 21:28:31 +0200

    [macro-test] updated babel.config.cjs to contain the default V2 addon publish configuration

--- tests/fixtures/v2-sample-addon-macro/babel.config.cjs
+++ tests/fixtures/v2-sample-addon-macro/babel.config.cjs
@@ -1,22 +1,12 @@
-const { buildMacros } = require('@embroider/macros/babel');
-const { babelCompatSupport, templateCompatSupport } = require('@embroider/compat/babel');
-
-const macros = buildMacros();
-
-// For scenario testing
-const isCompat = Boolean(process.env.ENABLE_COMPAT_BUILD);
-
 module.exports = {
   plugins: [
     [
       'babel-plugin-ember-template-compilation',
       {
-        transforms: [
-          ...(isCompat ? templateCompatSupport() : macros.templateMacros),
-        ],
+        targetFormat: 'hbs',
+        transforms: [],
       },
-    ],
-    ...(isCompat ? babelCompatSupport() : macros.babelMacros),
+    ]
   ],
 
   generatorOpts: {
